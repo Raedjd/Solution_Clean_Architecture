9f5546ddb5ca04333def58a91a3baa5e109ae0da
Raed <rjaidi@naxxum.fr>
2024-08-13 13:08:09 +0100

    Response And Response Handler in Clean Architecture done

--- SolutionProject.Api/SolutionProject.Application/Feature/Users/Queries/GetListUsers/GetUsersListQuery.cs
+++ SolutionProject.Api/SolutionProject.Application/Feature/Users/Queries/GetListUsers/GetUsersListQuery.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
+using SolutionProject.Application.Common.Bases;
 using SolutionProject.Application.DataTransfertObject;
 namespace SolutionProject.Application.Feature.Users.Queries.GetListUsers
 {
-    public class GetUsersListQuery : IRequest<List<UserDto>>
+    public class GetUsersListQuery : IRequest<Response<List<UserDto>>>
     {
     }
 }
--- SolutionProject.Api/SolutionProject.Application/Feature/Users/Queries/GetListUsers/GetUsersListQueryHandler.cs
+++ SolutionProject.Api/SolutionProject.Application/Feature/Users/Queries/GetListUsers/GetUsersListQueryHandler.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
+using SolutionProject.Application.Common.Bases;
 using SolutionProject.Application.Contracts.Persistence;
 using SolutionProject.Application.DataTransfertObject;
 
 namespace SolutionProject.Application.Feature.Users.Queries.GetListUsers
 {
-    public class GetUsersListQueryHandler : IRequestHandler<GetUsersListQuery, List<UserDto>>
+    public class GetUsersListQueryHandler :ResponseHandler, IRequestHandler<GetUsersListQuery, Response<List<UserDto>>>
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
@@ -15,11 +16,11 @@ namespace SolutionProject.Application.Feature.Users.Queries.GetListUsers
             _userRepository = userRepository;
             _mapper = mapper;
         }
-        public async Task<List<UserDto>> Handle(GetUsersListQuery request, CancellationToken cancellationToken)
+        public async Task<Response<List<UserDto>>> Handle(GetUsersListQuery request, CancellationToken cancellationToken)
         {
             var userList = await _userRepository.ListAsync(cancellationToken);
             var userListMapper = _mapper.Map<List<UserDto>>(userList);
-            return userListMapper;
+            return Success(userListMapper);
         }
     }
 }
